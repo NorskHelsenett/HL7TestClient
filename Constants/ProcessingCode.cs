b8d604a9d866d8054f268e6935e8343c35d94a54
Åsmund Eldhuset <aasmund.eldhuset@nhn.no>
2011-03-09 18:23:34 +0100

    Documenting ProcessingCode.

--- Constants/ProcessingCode.cs
+++ Constants/ProcessingCode.cs
@@ -2,6 +2,11 @@
 
 namespace HL7TestClient.Constants
 {
+    /// <summary>
+    /// A HL7 system always runs in one of the following modes.
+    /// Each incoming message must declare what the expected mode of the receiving system is (in a sense, what the intent of the message is).
+    /// The system will reject messages that indicate a mode that is different from the system's mode.
+    /// </summary>
     public static class ProcessingCode
     {
         public static CS Test() { return new CS("T"); }
