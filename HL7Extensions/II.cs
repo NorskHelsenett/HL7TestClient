f479ad6c437b8750904690315869642143cf2edd
Åsmund Eldhuset <aasmund.eldhuset@nhn.no>
2011-05-04 22:32:54 +0200

    Convenience constructors for II.

---
+++ HL7Extensions/II.cs
@@ -0,0 +1,27 @@
+﻿namespace HL7TestClient.PersonRegistry
+{
+    public partial class II : ANY
+    {
+        public II()
+        {
+        }
+
+        public II(string root, string extension)
+            : this()
+        {
+            this.root = root;
+            this.extension = extension;
+        }
+
+        public II(string root, string extension, string assigningAuthorityName)
+            : this(root, extension)
+        {
+            this.assigningAuthorityName = assigningAuthorityName;
+        }
+
+        public static II Unknown()
+        {
+            return new II {nullFlavor = NullFlavor.UNK, nullFlavorSpecified = true};
+        }
+    }
+}
